546d59b953e2f8531f5a630bc98323bbb7676774
7matic <7matic@protonmail.com>
2025-05-26 03:23:22 +0200

    Add SLURM scripts for performing finetuning and doing finetuned and instructed model evaluations.

---
+++ finetuning/arnes/scripts/run_slurm_finetuned_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=run-finetuned-gams9b-h100
+#SBATCH --partition=gpu
+#SBATCH --nodelist=gwn[08-10]
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=60G
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
+
+srun singularity exec --nv ./containers/container-pytorch-updated.sif \
+     python run_finetuned_model.py
